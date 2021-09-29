47a95b8fe6a9f9ea76840580e7fdc94db1e8d104
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-09-29 19:14:01 +0200

    QC Update the download example

--- examples/qc_download.py
+++ examples/qc_download.py
@@ -1,4 +1,5 @@
 # Standard Library
+import datetime
 import os
 
 # Requirements
@@ -9,17 +10,27 @@ import requests
 HOST = os.getenv('API_HOST', 'https://wsn.latice.eu')
 KEY = os.environ['API_KEY'] # This MUST be provided as an environment variable
 
-
 if __name__ == '__main__':
-    # Download the data
+    # These may be input to the program
+    name = 'sw-001'
+    since = datetime.datetime(2020, 1, 1)
+    until = datetime.datetime(2021, 1, 1)
+
+    # Variables
     headers = {'Authorization': f'Api-Key {KEY}'}
-    response = requests.get(f'{HOST}/api/qc/download/', headers=headers)
+    url = f'{HOST}/api/qc/download/{name}/'
+
+    # Filter by time range (optional)
+    since = int(since.timestamp())
+    until = int(until.timestamp())
+    url = f'{url}?time__gte={since}&time__lt={until}'
+
+    # Download the data
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
-    # Create dataframes from the data
+    # Create dataframe from the data
     data = response.json()
-    for node in data['results']:
-        print('name={name} lat={lat} lng={lng}'.format(**node))
-        df = pd.DataFrame.from_dict(node['data'])
-        print(df)
-        print()
+    df = pd.DataFrame.from_dict(data)
+    print(df)
+    print()
